32c1097acecd055c117ee4634f4f5490b0524ae0
KankshaP <42006771+KankshaP@users.noreply.github.com>
2020-05-11 18:56:56 +0530

    Integrated UI

--- music_gui.py
+++ music_gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
+import tkinter as tkinter
 import pandas as pd   #to extract data from exel
-
+import Recommenders
 
 def popularity():
    pop=Tk()
@@ -9,16 +10,18 @@ def popularity():
    pop.geometry("500x650")
 
    background = LabelFrame(pop, text="Trending songs of the week",font=("times new roman", 15, "italic"),fg="white",bg="#28373c")
-   background.place(x=10, y=10, width=480, height=630)
+   background.place(x=10, y=10, width=480, height=700)
    scrollbar = Scrollbar(pop)
    scrollbar.pack(side=RIGHT, fill=Y)
    # import dataset
    dataset = pd.read_csv("songs_merged_100000.csv")
    mylist = Listbox(pop, yscrollcommand=scrollbar.set, bg="#28373c", fg="white", font=("times new roman", 16, "italic"))
-   X = dataset.iloc[:50, 4:].values
+   pm = Recommenders.popularity_recommender_py()
+   pm.create(dataset, 'user_id', 'name')
+   X = pm.recommend(10).values
    print("___________________________________________________________")
-   for i in range(50):
-      mylist.insert(END, (i+1), X[i][0], X[0][1], X[0][2], X[0][4])
+   for i in range(10):
+      mylist.insert(END, "Trending #",(i+1),"Record title:",X[i][0],"No. of hits:",X[i][1])
       mylist.insert(END, "-----------------------------------------------------------------------")
 
    print(mylist)
@@ -34,18 +37,18 @@ def sim(T,pop):
    # import dataset
    dataset = pd.read_csv("songs_merged_100000.csv")
    mylist = Listbox(pop, yscrollcommand=scrollbar.set, bg="#28373c", fg="white", font=("times new roman", 16, "italic"))
-   X = dataset.iloc[:50, 4:].values
+   is_model = Recommenders.item_similarity_recommender_py()
+   is_model.create(dataset, 'user_id', 'name')
+   df = is_model.get_similar_items(song_id)
+   X = df.values
    print("___________________________________________________________")
-   for i in range(50):
-      mylist.insert(END, (i+1), X[i][0], X[0][1],  X[0][2], X[0][4])
+   for i in range(len(df)):
+      mylist.insert(END, (i+1), X[i][1])
       mylist.insert(END, "______________________________________________________________")
 
    print(mylist)
    mylist.place(x=20, y=155, width=450, height=480)
    scrollbar.config(command=mylist.yview, background="#28373c")
-
-def hel(c):
-   print(c)
 
 def similar():
    print("helo")
@@ -57,7 +60,7 @@ def similar():
    background = LabelFrame(pop, text="Songs matching to your taste of listening", font=("times new roman",18, "bold"), fg="white",
                            bg="#28373c")
    background.place(x=10, y=10, width=480, height=630)
-   background = Label(pop, text="⬖ Enter song you like",font=("times new roman", 15, "bold"), fg="white",bg="#28373c")
+   background = Label(pop, text="⬖ Enter songs you like",font=("times new roman", 15, "bold"), fg="white",bg="#28373c")
    background.place(x=20, y=50, width=200, height=30)
    T = Text(pop, height=2, width=38,font=("times new roman", 15))
    T.place(x=20,y=100)
@@ -67,12 +70,10 @@ def similar():
 
    pop.mainloop()
 
-
-
-
+   
 if __name__ == "__main__":
    # create a GUI window
-   root = Tk()
+   root = tkinter.Toplevel()
    root.configure(background="black")
 
    # set the title of GUI window
@@ -100,9 +101,8 @@ if __name__ == "__main__":
                       fg="white",
                       bd=1)
    track.place(x=15, y=755, width=640, height=50)
-   #sim=rec.item_similarity_recommender_py()
-   #listsong=sim.item_similarity_recommendations()
-
+   
 
 
    root.mainloop()
+ 
